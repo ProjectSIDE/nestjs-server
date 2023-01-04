3f85ea7460a37c3427abac4d383f41266c30dfcf
BreezeBM <seyya77@gmail.com>
2023-01-04 14:49:45 +0900

    feat: postgres 연결을 위한 옵션 값 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { PostgresConfig } from "../shared/database/ormconfig";
+
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: PostgresConfig.host,
+      port: PostgresConfig.port,
+      username: PostgresConfig.username,
+      password: PostgresConfig.password,
+      database: PostgresConfig.database,
+      entities: ['../src/domain/**/models/*.entity.ts']
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
